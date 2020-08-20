b3032c321ad6c39633e6397f2b1b82190e0b9415
assess15 <assess15@126.com>
2020-08-20 10:52:36 +0800

    完善 Builder模式

---
+++ JavaDesignPatternCode/src/com/assess15/design/creational/builder/demo1/Person.java
@@ -0,0 +1,43 @@
+package com.assess15.design.creational.builder.demo1;
+
+/**
+ * Builder模式
+ */
+public class Person {
+
+    final String age;
+    final String name;
+
+    Person(Builder builder) {
+        this.age = builder.age;
+        this.name = builder.name;
+    }
+
+    @Override
+    public String toString() {
+        return "BuilderDesign{" +
+                "age='" + age + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public static class Builder {
+        private String name;
+        private String age;
+
+        public Builder setName(String n) {
+            name = n;
+            return this;
+        }
+
+        public Builder setAge(String a) {
+            age = a;
+            return this;
+        }
+
+        public Person build() {
+            return new Person(this);
+        }
+    }
+
+}
---
+++ JavaDesignPatternCode/src/com/assess15/design/creational/builder/demo1/PersonTest.java
@@ -0,0 +1,14 @@
+package com.assess15.design.creational.builder.demo1;
+
+/**
+ * Builder模式
+ */
+public class PersonTest {
+    public static void main(String[] args) {
+        Person builder = new Person.Builder()
+                .setAge("25")
+                .setName("张三")
+                .build();
+        System.out.println(builder);
+    }
+}
