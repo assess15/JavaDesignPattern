c6deb503b1e38846f8e8cb74e72aa400df9f428b
lay <laychv@gmail.com>
2022-05-24 21:47:56 +0800

    update Builder

---
+++ code/src/com/laychv/design/creational/BuilderPattern/demo/Test.java
@@ -0,0 +1,11 @@
+package com.laychv.design.creational.BuilderPattern.demo;
+
+public class Test {
+
+    public static void main(String[] args) {
+        Builder builder = new MacBookProBuilder();
+        Director director = new Director(builder);
+        director.constructor("华硕主板", "三星显示器");
+        System.out.println("Computer info: " + builder.create().toString());
+    }
+}
