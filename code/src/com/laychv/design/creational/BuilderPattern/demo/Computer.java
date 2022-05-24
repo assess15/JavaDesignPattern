01130291285fad197e37a654b5799ead3bed21fe
lay <laychv@gmail.com>
2022-05-24 21:48:56 +0800

    update Builder

---
+++ code/src/com/laychv/design/creational/BuilderPattern/demo/Computer.java
@@ -0,0 +1,24 @@
+package com.laychv.design.creational.BuilderPattern.demo;
+
+public abstract class Computer {
+    protected String mBoard;
+    protected String mDisplay;
+    protected String mOS;
+
+    protected Computer() {
+    }
+
+    public void setBoard(String board) {
+        mBoard = board;
+    }
+
+    public void setDisplay(String display) {
+        mDisplay = display;
+    }
+
+    public abstract void setOS();
+
+    public String toString() {
+        return "Computer [mBoard= " + mBoard + ", mDisplay= " + mDisplay + ", mOS= " + mOS + "]";
+    }
+}
